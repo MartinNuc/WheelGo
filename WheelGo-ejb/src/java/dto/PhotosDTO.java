03a9a0dc72f35ae29cbd99139f96d2648c406d42
Martin Nuc <martin@nuc.cz>
2012-04-15 19:09:26 +0200

    PhotosDTO

---
+++ WheelGo-ejb/src/java/dto/PhotosDTO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author mist
+ */
+public class PhotosDTO {
+    private Integer idRole;
+    private String url;
+
+    public Integer getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(Integer idRole) {
+        this.idRole = idRole;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+       
+}
