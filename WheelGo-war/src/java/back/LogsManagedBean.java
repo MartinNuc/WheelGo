8a4663209684f0746572ac3474b9cc0af17e40f1
Martin Nuc <martin@nuc.cz>
2012-04-16 21:03:29 +0200

    Logs Wrapper

--- WheelGo-war/src/java/back/LogsManagedBean.java
+++ WheelGo-war/src/java/back/LogsManagedBean.java
@@ -6,11 +6,16 @@ package back;
 
 import dto.LogDTO;
 import ejb.facades.implementation.LogFacade;
-import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
+import ejb.facades.implementation.ReportFacade;
+import ejb.facades.implementation.UserFacade;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+import wrappers.LogWrapper;
 
 /**
  *
@@ -21,11 +26,30 @@ import javax.faces.bean.ManagedBean;
 @SessionScoped
 public class LogsManagedBean implements Serializable {
     @EJB
-    private LogFacade logsFacade;
+    private LogFacade logFacade;
+    @EJB
+    private UserFacade userFacade;
+    @EJB
+    private ReportFacade reportFacade;
     
     private LogDTO log;
 
     /** Creates a new instance of LogsManagedBean */
     public LogsManagedBean() {
+    }
+    
+    public void removeLog(LogWrapper log) {
+        logFacade.remove(log.getDto());
+    }
+
+    public List<LogWrapper> getLogs() {
+        List<LogWrapper> list = new ArrayList<LogWrapper>();
+        List<LogDTO> allLogs = logFacade.getAll();
+        if (allLogs != null) {
+            for (LogDTO l : allLogs) {
+                list.add(new LogWrapper(l, userFacade, reportFacade));
+            }
+        }
+        return list;
     }
 }
