a59414b9472a688e3dbb6638d6c16332c0bc7dbf
vavravl1 <vladislav.vavra@gmail.com>
2012-04-16 20:05:13 +0200

    Report tvoritko

---
+++ WheelGo-ejb/src/java/ejb/CreateReport.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author vlada
+ */
+@Stateful
+public class CreateReport implements CreateReportLocal {
+
+    private static final int TYPE_TIP = 1;
+    private static final int TYPE_PLACE = 2;
+    private static final int TYPE_PROBLEM = 3;
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+}
